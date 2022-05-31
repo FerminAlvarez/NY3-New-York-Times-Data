b00315960dcc72c3684785205c8f703534c22eb9
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-05-31 17:41:08 -0300

    Remuevo isLocallyStored de NytArtistInfo

--- src/main/java/ayds/ny3/newyorktimes/NytArtistInfo.kt
+++ src/main/java/ayds/ny3/newyorktimes/NytArtistInfo.kt
@@ -1,16 +1,7 @@
 package ayds.ny3.newyorktimes
 
-
-interface ArtistInfo {
-    val artistName: String
-    val artistInfo: String
-    val artistURL: String
-    var isLocallyStored: Boolean
-}
-
 data class NytArtistInfo(
-    override val artistName: String,
-    override val artistInfo: String,
-    override val artistURL: String,
-    override var isLocallyStored: Boolean = false
-) : ArtistInfo
+    val artistName: String,
+    val artistInfo: String,
+    val artistURL: String,
+)
