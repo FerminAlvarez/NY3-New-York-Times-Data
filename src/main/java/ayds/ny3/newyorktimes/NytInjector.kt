6bcedf3995fa19ecd76de3821ef81550cf71d55a
gonzariquelme6 <gonzariquelme66@gmail.com>
2022-05-31 17:59:18 -0300

    Agrego inyeccion de dependencias para nyTimesAPI

--- src/main/java/ayds/ny3/newyorktimes/NytInjector.kt
+++ src/main/java/ayds/ny3/newyorktimes/NytInjector.kt
@@ -1,9 +1,27 @@
 package ayds.ny3.newyorktimes
 
 import ayds.ny3.newyorktimes.article.JsonToArtistInfoResolver
+import ayds.ny3.newyorktimes.article.NYTimesAPI
 import ayds.ny3.newyorktimes.article.NytArticleServiceImpl
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val NYT_API_URL = "https://api.nytimes.com/svc/search/v2/"
 
 object NytInjector {
 
-    val nytArticleService: NytArticleService = NytArticleServiceImpl(JsonToArtistInfoResolver())
+    val nyTimesAPI: NYTimesAPI = createRetrofit()
+
+    private fun createRetrofit(): NYTimesAPI {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(NYT_API_URL)
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+        return retrofit.create(NYTimesAPI::class.java)
+    }
+
+    val nytArticleService: NytArticleService = NytArticleServiceImpl(
+        JsonToArtistInfoResolver(),
+        nyTimesAPI
+    )
 }
