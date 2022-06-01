3d8e860ad721bda4080f78245dbb682e7b97cfe1
Fermin <feeralvarez013@gmail.com>
2022-06-01 10:22:40 -0300

    Agregado nytLogo

--- src/main/java/ayds/ny3/newyorktimes/NytArtistInfo.kt
+++ src/main/java/ayds/ny3/newyorktimes/NytArtistInfo.kt
@@ -4,4 +4,5 @@ data class NytArtistInfo(
     val artistName: String,
     val artistInfo: String,
     val artistURL: String,
+    val nytLogo: String = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
 )
