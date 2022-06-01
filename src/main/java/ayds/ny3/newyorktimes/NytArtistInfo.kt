2888a498da452cfc7456163ca1e8444f88381277
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-06-01 17:29:48 -0300

    extrayendo la url del logo

--- src/main/java/ayds/ny3/newyorktimes/NytArtistInfo.kt
+++ src/main/java/ayds/ny3/newyorktimes/NytArtistInfo.kt
@@ -4,5 +4,10 @@ data class NytArtistInfo(
     val artistName: String,
     val artistInfo: String,
     val artistURL: String,
-    val nytLogo: String = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
-)
+    val nytLogo: String = NYT_LOGO_URL,
+) {
+    companion object {
+        private const val NYT_LOGO_URL =
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVioI832nuYIXqzySD8cOXRZEcdlAj3KfxA62UEC4FhrHVe0f7oZXp3_mSFG7nIcUKhg&usqp=CAU"
+    }
+}
